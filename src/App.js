88c75566bf243dbd644b61f3c186ab6e380df48a
weiner-rezcue98 <pedroweinerbr@gmail.com>
2023-10-20 23:52:22 -0300

    up

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import GlobalStyle from "./styles/global";
+import RoutesApp from "./routes";
+
+const App = () => {
+    return (
+      <>
+        <div>App</div>
+        <RoutesApp />
+        <GlobalStyle />
+      </>
+    );
+};
+
+export default App
