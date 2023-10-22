f500c76a65d3fbd1da1576ee1417579ce9375282
weiner-rezcue98 <pedroweinerbr@gmail.com>
2023-10-22 18:56:19 -0300

    Update index.js

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { useNavigate } from "react-router";
-import Button from "../../components/Button/styles";
+import Button from "../../components/Button"; // Corrigindo o caminho de importação
 import useAuth from "../../hooks/useAuth";
 import * as C from "./styles";
 
 const Home = () => {
+    const { signout } = useAuth();
+    const navigate = useNavigate();
 
-    
-    return <div>Home</div>;
+    return (
+        <C.Container>
+            <C.Title>Home</C.Title>
+            <Button Text="Sair" onClick={() => [signout(), navigate("/")]} >
+                Sair
+            </Button>
+        </C.Container>
+    );
 };
 
 export default Home;
