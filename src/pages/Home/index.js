8eab7b7f9c125392f9001cf6b142ce27bb5c7372
weiner-rezcue98 <pedroweinerbr@gmail.com>
2023-10-22 13:52:37 -0300

    update

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React from "react";
+import { useNavigate } from "react-router";
+import Button from "../../components/Button/styles";
+import useAuth from "../../hooks/useAuth";
+import * as C from "./styles";
 
 const Home = () => {
+
+    
     return <div>Home</div>;
 };
 
